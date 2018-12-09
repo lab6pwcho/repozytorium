58a3a49f12a05c66049d36f463ff5c716600197c
lab6pwcho <45427971+lab6pwcho@users.noreply.github.com>
2018-12-09 17:59:33 +0100

    Update plik.java

--- plik.java
+++ plik.java
@@ -4,7 +4,7 @@ public class plik {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:mysql://db:10.0.10.3:3306/zad1";
 
-   static final String USER = "pkalasa";
+   static final String USER = "pwozniacka";
    static final String PASS = "password";
    
    public static void main(String[] args) {
